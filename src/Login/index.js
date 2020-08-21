6eb48a183e2f560f1b259ad1275108f41be4f767
Dominik Feger <dominik.feger@abf.ch>
2020-08-21 13:57:16 +0200

    added env and org

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,118 +1,155 @@
 /* eslint-disable camelcase */
-import React from 'react'
-import { Button,
-    Container,
-    TextField,
-    Grid,
-    Paper,
-    Typography } from '@material-ui/core'
-import useStyles from './index.styles'
-import axios from 'axios'
-import storageHandler from '../storageHandler'
+import React from "react";
+import {
+  Button,
+  Container,
+  TextField,
+  Grid,
+  Paper,
+  Typography,
+} from "@material-ui/core";
+import useStyles from "./index.styles";
+import axios from "axios";
+import storageHandler from "../storageHandler";
 
 const Login = () => {
-    /*const {
+  /*const {
         actions: { login },
         state: { loggingIn },
     } = useBasicAuth()*/
-    console.log(storageHandler)
-    const classes = useStyles()
-const loggingIn = false
-const login = (e) => {
-    console.log(e)
-e.preventDefault()
+  console.log(storageHandler);
+  const classes = useStyles();
+  const loggingIn = false;
 
-    axios.post('https://osxdev.abf.local:443/api/login', {
-        username: 'dfeger',
-        password: 'manager'
+  storageHandler.set(
+    "cubelineToken",
+    "ben ishc recht geil man",
+    new Date() + 360000
+  );
+  console.log(storageHandler.get("cubelineToken"));
+  const login = (e) => {
+    console.log(e);
+    e.preventDefault();
+    //login then orgs then env
+
+    axios
+      .post("https://osxdev.abf.local:443/api/login", {
+        username: "dfeger",
+        password: "manager",
       })
       .then(function (response) {
-        console.log(response)
-        storageHandler.set('cubelineToken', response.data.accessToken , +response.data.expires)
+        storageHandler.set(
+          "cubelineToken",
+          response.data.accessToken,
+          +response.data.expires
+        );
 
+        console.log(storageHandler.get("cubelineToken"));
+
+        axios
+          .get("https://osxdev.abf.local:443/api/env/orgs", {
+            withCredentials: true,
+            headers: {
+              Authorization: `Bearer ${response.data.accessToken}`,
+            },
+          })
+          .then(function (response) {
+            console.log(response);
+            storageHandler.set("orgs", response.data);
+
+            axios
+              .post(
+                "https://osxdev.abf.local:443/api/env",
+                {
+                  orgId: 12,
+                  isRest: false,
+                },
+                {
+                  headers: {
+                    Authorization: `Bearer ${storageHandler.get(
+                      "cubelineToken"
+                    )}`,
+                  },
+                }
+              )
+              .then(function (response) {
+                console.log(response);
+                storageHandler.set("env", response.data);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
       })
       .catch(function (error) {
-        console.log(error)
-      })
-      console.log(storageHandler.get('cubelineToken'))
-}
-    return (
-                <Container
-                    maxWidth="xs"
-                    disableGutters
+        console.log(error);
+      });
+  };
+  return (
+    <Container maxWidth="xs" disableGutters>
+      <Paper elevation={2} className={classes.papier}>
+        <Grid
+          item
+          container
+          spacing={0}
+          justify="flex-end"
+          alignItems="flex-end"
+        >
+          <form onSubmit={login}>
+            <div className={classes.form}>
+              <div className={classes.centered}>
+                <Typography variant="caption" gutterBottom>
+                  Im just a PoC to test new cookies bruh
+                </Typography>
+                <TextField
+                  label="Username"
+                  variant="outlined"
+                  size="small"
+                  color="primary"
+                  fullWidth
+                  id="username"
+                  name="username"
+                  autoComplete="username"
+                  disabled={loggingIn}
+                  required
+                />
+                <br />
+                <TextField
+                  label="Password"
+                  name="password"
+                  type="password"
+                  size="small"
+                  autoComplete="current-password"
+                  fullWidth
+                  disabled={loggingIn}
+                />
+              </div>
+              <div className={classes.actions}>
+                <Button
+                  type="submit"
+                  label={loggingIn ? <Loader relative size={1} /> : "LOGIN"}
+                  name="submit"
+                  color="primary"
+                  variant="contained"
+                  disabled={loggingIn}
                 >
-                    <Paper
-                        elevation={2}
-                        className={classes.papier}
-                    >
-                        <Grid
-                            item
-                            container
-                            spacing={0}
-                            justify="flex-end"
-                            alignItems="flex-end"
-                        >
-                            <form
-                                onSubmit={login}
-                            >
-                                <div className={classes.form}>
-                                    <div className={classes.centered}>
-                                        <Typography variant="caption" gutterBottom>
-                                            Im just a PoC to test new cookies bruh
-                                        </Typography>
-                                        <TextField
-                                            label="Username"
-                                            variant="outlined"
-                                            size="small"
-                                            color="primary"
-                                            fullWidth
-                                            id="username"
-                                            name="username"
-                                            autoComplete="username"
-                                            disabled={loggingIn}
-                                            required
-                                        />
-                                        <br />
-                                        <TextField
-                                            label="Password"
-                                            name="password"
-                                            type="password"
-                                            size="small"
-                                            autoComplete="current-password"
-                                            fullWidth
-                                            disabled={loggingIn}
-                                        />
-                                    </div>
-                                    <div className={classes.actions}>
-                                        <Button
-                                            type="submit"
-                                            label={
-                                                loggingIn ? (
-                                                    <Loader
-                                                        relative
-                                                        size={1}
-                                                    />
-                                                ) : (
-                                                    'LOGIN'
-                                                )
-                                            }
-                                            name="submit"
-                                            color="primary"
-                                            variant="contained"
-                                            disabled={loggingIn}
-                                        >SUBMIT</Button>
-                                    </div>
-                                </div>
-                            </form>
-                        </Grid>
-                        <div className={classes.copyright}>
-                            <Typography variant="caption">
-                                Copyright ©2020 ABF Informatik AG
-                            </Typography>
-                        </div>
-                    </Paper>
-                </Container>
-    )
-}
+                  SUBMIT
+                </Button>
+              </div>
+            </div>
+          </form>
+        </Grid>
+        <div className={classes.copyright}>
+          <Typography variant="caption">
+            Copyright ©2020 ABF Informatik AG
+          </Typography>
+        </div>
+      </Paper>
+    </Container>
+  );
+};
 
-export default Login
+export default Login;
--- src/storageHandler/index.js
+++ src/storageHandler/index.js
@@ -1,12 +1,10 @@
-const engine = require('store/src/store-engine')
+const engine = require("store/src/store-engine");
 const storages = [
-    require('store/storages/all'),
-	require('store/storages/cookieStorage')
-]
-const plugins = [
-	require('store/plugins/expire')
-]
+  require("store/storages/sessionStorage"),
+  require("store/storages/cookieStorage"),
+];
+const plugins = [require("store/plugins/expire")];
 
-const storageHandler = engine.createStore(storages, plugins)
+const storageHandler = engine.createStore(storages, plugins);
 
-export default storageHandler
+export default storageHandler;
